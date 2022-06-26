f01583a1d13608b3f22c3825856904405186155a
edmariooliver <edmariooliver@outlook.com>
2022-06-26 00:26:04 -0300

    Add content noticias

--- src/Modules/Commands/Noticias.php
+++ src/Modules/Commands/Noticias.php
@@ -10,6 +10,8 @@ class Noticias extends Command
     public function index()
     {
         $this->wssConnection->send(["msg"=>"Buscando noticias", "name"=>"BOT [Rafão]"]);
+        $content = file_get_contents("https://api-aleatoria.herokuapp.com/api/noticias/todas");
+        $this->wssConnection->send(["msg"=>$content, "name"=>"BOT [Rafão]"]);
         return json_encode(["Certo"]);  
     }
 }
