85a6245a5d918e2d5f5181399754f72e8e7a829b
edmariooliver <edmariooliver@outlook.com>
2022-06-26 00:18:22 -0300

    Mudando arquivo rotas

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-require_once(__DIR__."/vendor/autoload.php");
+require(__DIR__."/vendor/autoload.php");
 
-use Marrios\Router\HttpRouter;
-
-$router = new HttpRouter();
-
-$router->get("/noticias", [App\Modules\Commands\Noticias::class, "index"])->run();
-
-
-echo  "Ta on";
+require(__DIR__."RoutesCommand.php");
